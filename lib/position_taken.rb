1554f0823642b76b7486aebe690f969bff7bcaa8
Glenn Canavan <gcana001@fiu.edu>
2019-04-03 01:19:01 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,13 +1,6 @@
-def input_to_index(user_input)
-  user_input.to_i - 1
-end
-
-def valid_move?(board, index)
-  if index.between?(0,8)
-    if !position_taken?(board, index)
-      true
-    else
-      false
-    end
+def position_taken?(board, location)
+  board[location] != " " && board[location] != ""
+  if position_taken?(board, index)
+    false
   end
 end
