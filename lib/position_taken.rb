2240d8d4ff36c5c9ec1b7597fac8a70b16985001
Glenn Canavan <gcana001@fiu.edu>
2019-04-03 01:16:38 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,33 @@
-def position_taken?(board, location)
-  board[location] != " " && board[location] != ""
+def input_to_index(user_input)
+  user_input.to_i - 1
 end
 
 def valid_move?(board, index)
-  index.between?(0,8) && !position_taken?(board, index)
+  if index.between?(0,8)
+    if !position_taken?(board, index)
+      true
+    end
+  end
+
+  # position.to_i.between?(0,8) && !position_taken?(board, position.to_i-1)
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def position_taken?(board, index)
+  board[index] != " "
+end
+
+def move(board, index, current_player = "X")
+  board[index] = current_player
 end
